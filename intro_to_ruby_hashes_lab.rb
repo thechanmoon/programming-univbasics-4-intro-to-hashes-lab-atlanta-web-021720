4d442c55db652805a13b2d45c7b2830c18770170
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-13 21:38:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,10 +5,12 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  return {name : 'tiburon'}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  return {name : 'Grace Hopper'}
 end
 
 def id_generator
@@ -22,8 +24,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  return hash[key]
   # return the correct value using the hash and key parameters
+    return hash[key]
 end
 
 def update_counting_hash(hash, key)
